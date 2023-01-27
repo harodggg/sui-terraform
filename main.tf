36250cbe1b3570f8168a346ccefe083d20c0f5e9
harodggg <haroldtiansheng@gmail.com>
2023-01-28 03:02:29 +0800

    add somethings

--- main.tf
+++ main.tf
@@ -9,26 +9,30 @@ terraform {
 
 provider "google" {
   credentials = file(var.credentials_file)
-  
-
-  project = var.project
-  region  = var.region
-  zone    = var.zone
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
 }
 
+
 resource "google_compute_network" "sui_network" {
-  name = "sui-network"
+  name                    = "sui-network"
   auto_create_subnetworks = "true"
 
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "sui-instance"
+
+resource "google_compute_instance" "sui_instance" {
+  count        = 5
+
+  name         = "sui-instance-${count.index}"
   machine_type = "f1-micro"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      type  = "pd-standard"
+      size  = 1000 * 1.024                 // 1000M * 1.024 * 200 = 200G
     }
   }
 
